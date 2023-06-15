cc1b2ee69291eb12e30ed77ab608f37f243a0753
rosierludivine <ludivine.rosier38@gmail.com>
2023-06-15 15:19:01 +0200

    Boxeur en dynamique

--- Projet_Exam/Projet_Exam.cpp
+++ Projet_Exam/Projet_Exam.cpp
@@ -13,5 +13,10 @@ int main()
     cout << "Boxeur 1: " << box_1.GetNom() << ", et a " << box_1.GetPoids() << "kg." << " Il se trouve à: " << &box_1 << endl;
     
 
+    // en dynamique 
+    Boxeur* box_2; 
+    box_2 = new Boxeur("Charline", 100);
+    cout << "Boxeur 2: " << box_2->GetNom() << ", et a " << box_2->GetPoids() << "kg." << " Il se trouve à: " << &box_2 << endl;
+
 
 };
